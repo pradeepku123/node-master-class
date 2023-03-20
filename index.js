31e5b9eac2ff7d91368da703f16b1f2c37fa580e
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2023-03-20 07:53:13 +0530

    FS

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import { sum, diff } from "./lib.js";
+import fs from "fs";
 console.log(`Welcome to Node MasterClassd `);
 
 console.log(sum(1, 2), diff(1, 2));
+
+fs.readFile("eg.txt", "utf-8", (err, txt) => {
+  console.log(txt);
+});
