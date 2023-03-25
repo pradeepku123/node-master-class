9fd02bd48787763cc9fcc75967fedb0ac816c9fc
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2023-03-25 16:28:47 +0530

    http

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-import data from "./data.json" assert { type: "json" };
+/**
+ * Index.js entry point from node Project
+ */
+import http from "http";
+import fs from "fs";
 
-console.log(data);
+const data = fs.readFileSync("index.html", "utf-8");
+
+const server = http.createServer((req, res) => {
+  console.log(`${req.url} ${req.method}`);
+  res.writeHead(200, { "content-type": "text/html" });
+  res.end(data);
+});
+
+server.listen(3000, () => {
+  console.log(`Server listening on 3000`);
+});
